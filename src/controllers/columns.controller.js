3e7bfb99d5e0695a29931dce0ae2c36bcd73e3e1
Jang Sihun <chocoj1123@naver.com>
2023-08-07 20:08:32 +0900

    docs:branch rename

---
+++ src/controllers/columns.controller.js
@@ -0,0 +1,3 @@
+class ColumnsController {}
+
+export default ColumnsController;
---
+++ src/routes/columns.routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// router.post('/api/columns', auth.verify, ColumsController.createColums);
+
+// router.get('/api/columns', );
+
+// router.put('/api/columns/:columnId', auth.verify, ColumsController.updateColums);
+
+// router.delete('/api/columns/:columnId', auth.verify, ColumsController.deleteColums);
+
+export default router;
