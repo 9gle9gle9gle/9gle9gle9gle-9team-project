6a2cb95b4444aad2c9e961f6c21c9ecf42ffd8ba
dayounglee <verdantjuly@kakao.com>
2023-08-09 08:19:55 +0900

    feat : 보드 전체 조회 API
    boards.controller.js : 17
    boards.repository.js : 12
    boards.service.js : 25

--- src/services/boards.service.js
+++ src/services/boards.service.js
@@ -22,6 +22,30 @@ class BoardsService {
       return messages.status400();
     }
   };
+  getBoards = async userId => {
+    try {
+      const getBoards = await this.boardsRepository.getBoards(userId);
+      if (!getBoards)
+        return {
+          status: 400,
+          message: '보드 전체 조회에 실패하였습니다.',
+          getBoards: null,
+        };
+      else
+        return {
+          status: 200,
+          message: '보드 전체 조회에 성공하였습니다.',
+          getBoards,
+        };
+    } catch (err) {
+      console.log(err);
+      return {
+        status: 400,
+        message: '보드 전체 조회에 실패하였습니다.',
+        getBoards: null,
+      };
+    }
+  };
 }
 
 export default BoardsService;
