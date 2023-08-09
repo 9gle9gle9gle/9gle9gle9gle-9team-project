9bbb30b698bac3957c13e9481827c2d9177cc3ab
dayounglee <verdantjuly@kakao.com>
2023-08-09 10:43:14 +0900

    feat : 보드 수정 API
    boards.controller.js : 31
    boards.repository.js : 47
    boards.service.js.  87
    boards.route.js : 10

--- src/routes/boards.routes.js
+++ src/routes/boards.routes.js
@@ -7,5 +7,6 @@ const boardsController = new BoardsController();
 router.post('/boards', boardsController.makeBoard);
 router.get('/boards', boardsController.getBoards);
 router.get('/boards/:boardId', boardsController.showABoard);
+router.patch('/boards/:boardId', boardsController.updateBoard);
 
 export default router;
