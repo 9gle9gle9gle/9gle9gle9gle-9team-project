df1592048dbb9f27788d6606da7b134f9df6e6f9
dayounglee <verdantjuly@kakao.com>
2023-08-10 21:13:12 +0900

    feat : 카드 조회 프론트엔드

---
+++ public/js/boardlist.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+  boardlist();
+});
+
+async function boardlist() {
+  const response = await fetch(`http://localhost:3000/api/boards`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: sessionStorage.getItem('Authorization'),
+    },
+  });
+
+  const result = await response.json();
+  const tempHtml = result.getBoards
+    .map(board => {
+      let boardColor;
+      if (board.boardColor == 0) {
+        boardColor = 'red';
+      } else if (board.boardColor == 1) {
+        boardColor = 'orange';
+      } else if (board.boardColor == 2) {
+        boardColor = 'yellow';
+      } else if (board.boardColor == 3) {
+        boardColor = 'green';
+      } else if (board.boardColor == 4) {
+        boardColor = 'blue';
+      } else if (board.boardColor == 5) {
+        boardColor = 'purple';
+      }
+
+      return `<div style = "border : 2px solid ${boardColor}" onclick= "moveToBoard(${board.boardId})">${board.boardName}</div>`;
+    })
+    .join('');
+
+  const boardlist = document.querySelector('#boardlist');
+  boardlist.innerHTML = tempHtml;
+  console.log(result.message);
+}
+
+function moveToBoard(boardId) {
+  sessionStorage.setItem('boardId', boardId);
+  location.href = 'board.html';
+}
--- public/js/card.js
+++ public/js/card.js
@@ -1,17 +1,45 @@
-function showACard() {
-  async function viewABoard() {
-    const boardId = 1;
-    const response = await fetch(
-      `http://localhost:3000/api/boards/${boardId}/columns`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: sessionStorage.getItem('Authorization'),
-        },
-      },
-    );
+showACard();
+async function showACard() {
+  const cardId = 22;
+  const response = await fetch(`http://localhost:3000/api/cards/${cardId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: sessionStorage.getItem('Authorization'),
+    },
+  });
 
-    const result = await response.json();
+  const result = await response.json();
+  let cardColor;
+  if (result.message.cardColor == 0) {
+    cardColor = 'red';
+  } else if (result.message.cardColor == 1) {
+    cardColor = 'orange';
+  } else if (result.message.cardColor == 2) {
+    cardColor = 'yellow';
+  } else if (result.message.cardColor == 3) {
+    cardColor = 'green';
+  } else if (result.message.cardColor == 4) {
+    cardColor = 'blue';
+  } else if (result.message.cardColor == 5) {
+    cardColor = 'purple';
   }
+  const today = new Date();
+  const endDate = new Date(result.message.endAt);
+  const leftDate = Math.floor((endDate.getTime() - today.getTime()) / 86400000);
+  let leftalert;
+  if (leftDate > 0) {
+    leftalert = `마감일까지 ${leftDate}일 남았습니다.`;
+  } else {
+    leftalert = '마감일이 지났습니다.';
+  }
+  const tempHtml = `<div style = "border : 2px solid ${cardColor}">
+                        <h2>${result.message.cardName}</h2>
+                        <div>${result.message.cardContent}</div>
+                        <div>${result.message.endAt.slice(0, 10)}</div>
+                        <div>${leftalert}</div>
+                    </div>`;
+
+  const card = document.querySelector('#card');
+  card.innerHTML = tempHtml;
 }
