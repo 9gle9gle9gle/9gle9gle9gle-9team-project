335165515ef26d2ade48cb79f9a19ef2a09b0358
Jang Sihun <chocoj1123@naver.com>
2023-08-11 01:03:57 +0900

    fix : 보더

--- public/js/board.js
+++ public/js/board.js
@@ -19,36 +19,39 @@ async function viewABoard() {
   const tempHtml = result.columns
     .map(column => {
       showCards(boardId, column.columnId);
-      return `<div class = "cardBox">
-                  <h2>${column.columnName}</h2>
-                  <div class = "cardslist" id ="cardslist${column.columnId}"></div>
-                  <div class = "cardCreate">
-                      <input type="hidden" value="1" id="boardId"></input>
-                      <input type="hidden" value="1" id="columnId"></input>
-
-                      <label>카드 제목</label>
-                        <input type="text" id="cardName${column.columnId}"/>
-
-                      <label>카드 내용</label>
-                        <input type="text" id="cardContent${column.columnId}" />
-
-                      <label>카드 색상</label>
-                      <select id = "cardColor${column.columnId}">
-                        <option selected>-- 선택해 주세요 --</option>
-                          <option value="0">red</option>
-                          <option value="1">orange</option>
-                          <option value="2">yellow</option>
-                          <option value="3">green</option>
-                          <option value="4">blue</option>
-                          <option value="5">purple</option>
-                      </select>
-
-                      <label>마감 날짜</label>
-                        <input type="date" id="endAt${column.columnId}" />
-                      
-                      <button type ="button" onclick="makeCard(${boardId}, ${column.columnId})">생성</button>
-                    </div>
-                <div>`;
+      return `<div class="cardBox">
+      <div class="title1"><p>${column.columnName}</p>
+      <button class="deleteColumnButton" onclick="deleteColumn(${column.columnId})">삭제</button></div>
+      <div class="cardone">
+        <div class="cardCreate">
+          <input type="hidden" value="1" id="boardId">
+          <input type="hidden" value="1" id="columnId">
+          <div class="card">
+            <input type="text" id="cardName${column.columnId}" />
+            <button type="button" onclick="openModal(${boardId}, ${column.columnId})">생성</button>
+            <div id="myModal" class="modal">
+              <div class="modal-content">
+                <span class="close" onclick="closeModal1()">&times;</span>
+                <select id="cardColor${column.columnId}">
+                  <option selected>-- 카드 색상 --</option>
+                  <option value="0">red</option>
+                  <option value="1">orange</option>
+                  <option value="2">yellow</option>
+                  <option value="3">green</option>
+                  <option value="4">blue</option>
+                  <option value="5">purple</option>
+                </select>
+              <input type="date" id="endAt${column.columnId}" />
+                
+                <textarea type="text" class ="cardContent" id="cardContent${column.columnId}" placeholder="내용을 입력해주세요"/></textarea>
+                <button type="button" class = "createBTN" onclick="makeCard(${boardId}, ${column.columnId})">카드 생성</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="cardslist" id="cardslist${column.columnId}"></div>
+      </div>
+    </div>`;
     })
     .join('');
   const addTodoListDiv = document.querySelector('#addTodoListDiv');
@@ -100,19 +103,19 @@ async function showCards(boardId, columnId) {
     .map(item => {
       let cardColor;
       if (item.cardColor == 0) {
-        cardColor = 'red';
+        cardColor = 'tomato';
       } else if (item.cardColor == 1) {
-        cardColor = 'orange';
+        cardColor = 'goldenrod';
       } else if (item.cardColor == 2) {
-        cardColor = 'yellow';
+        cardColor = 'lemonchiffon';
       } else if (item.cardColor == 3) {
         cardColor = 'green';
       } else if (item.cardColor == 4) {
-        cardColor = 'blue';
+        cardColor = 'lightcyan';
       } else if (item.cardColor == 5) {
-        cardColor = 'purple';
+        cardColor = 'thistle';
       }
-      return `<div style = "border : 2px solid ${cardColor}" onclick="location.href='signup.html'" >
+      return `<div style="background: ${cardColor}" onclick="openCardModal('${item.cardName}', '${item.cardContent}', '${item.endAt}')">
               <h2>${item.cardName}</h2>
             </div>`;
     })
@@ -120,7 +123,6 @@ async function showCards(boardId, columnId) {
 
   const target = document.querySelector(`#cardslist${columnId}`);
   target.innerHTML = tempHtml;
-
   console.log(result.message);
   return;
 }
@@ -148,4 +150,62 @@ async function createColumn() {
   } catch (error) {
     console.error('컬럼 생성 중 오류 발생:', error);
   }
+}
+/////////////////////////////////////
+// 모달 창 열기
+function openModal(boardId, columnId) {
+  const modal = document.getElementById('myModal');
+  modal.style.display = 'block';
+
+  // 생성 버튼에 boardId와 columnId 전달
+  const createButton = modal.querySelector('button');
+  createButton.setAttribute('onclick', `makeCard(${boardId}, ${columnId})`);
+}
+
+function openCardModal(cardName, cardContent, endAt) {
+  const modal = document.querySelector('.cardModal');
+  const modalContent = modal.querySelector('.cardmodal-content');
+
+  modalContent.innerHTML = `
+    <span class="close" onclick="closeModal()">&times;</span>
+    <h2>${cardName}</h2>
+    <p>마감 날짜: ${endAt}</p>
+    <div class = "cardcontent"><p>${cardContent}</p></div>
+    <div class="commentSection">
+      <div class="comments">
+        <!-- 댓글이 여기에 동적으로 추가될 것입니다 -->
+      </div>
+      <textarea id="commentTextarea" placeholder="댓글을 입력하세요"></textarea>
+      <button onclick="addComment()">입력</button>
+    </div>
+  `;
+
+  modal.style.display = 'block';
+}
+
+// 댓글 기능
+
+function addComment() {
+  const commentTextarea = document.getElementById('commentTextarea');
+  const comment = commentTextarea.value;
+
+  if (comment.trim() !== '') {
+    const commentsSection = document.querySelector('.comments');
+    const commentElement = document.createElement('div');
+    commentElement.classList.add('comment');
+    commentElement.textContent = comment;
+    commentsSection.appendChild(commentElement);
+
+    commentTextarea.value = ''; // 댓글 입력창 비우기
+  }
+}
+
+// 모달 창 닫기
+function closeModal1() {
+  const modal = document.getElementById('myModal');
+  modal.style.display = 'none';
+}
+function closeModal() {
+  const modal = document.querySelector('.cardModal');
+  modal.style.display = 'none';
 }
