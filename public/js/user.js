caed00730ff99fc7d1cd196c8d607b4be5615838
yeonoh <beky010101@gmail.com>
2023-08-12 10:20:40 +0900

    Feat: 로그아웃 연결 완성 아님

--- public/js/user.js
+++ public/js/user.js
@@ -1,9 +1,14 @@
+// 로그아웃
 async function logout() {
-  const response = fetch(`http://localhost:3000/api/logout`, {
+  const response = await fetch(`http://localhost:3000/api/logout`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
 
-  const result = (await response).json();
+  const result = await response.json();
+  console.log(result);
 
   if (response.ok) {
     console.log(result.message);
@@ -16,6 +21,7 @@ async function logout() {
   }
 }
 
+// 회원 탈퇴
 async function deleteUser() {
   const response = await fetch(`http://localhost:3000/api/signout`, {
     method: 'DELETE',
