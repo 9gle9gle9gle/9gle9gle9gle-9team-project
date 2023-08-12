3e8609e58e1dd0d9e28bac4c7a5a4292c0c0bca6
yeonoh <beky010101@gmail.com>
2023-08-12 09:56:47 +0900

    Feat: 회원탈퇴 연결 완료

--- public/js/user.js
+++ public/js/user.js
@@ -1,3 +1,40 @@
-function logout() {}
+async function logout() {
+  const response = fetch(`http://localhost:3000/api/logout`, {
+    method: 'POST',
+  });
 
-function deleteUser() {}
+  const result = (await response).json();
+
+  if (response.ok) {
+    console.log(result.message);
+    alert(result.message);
+
+    window.location.href = 'http://localhost:3000';
+  } else {
+    console.log(result.message);
+    alert(result.message);
+  }
+}
+
+async function deleteUser() {
+  const response = await fetch(`http://localhost:3000/api/signout`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: sessionStorage.getItem('Authorization'),
+    },
+    body: JSON.stringify({ deletedAt: new Date() }),
+  });
+
+  const result = await response.json();
+
+  if (response.ok) {
+    console.log(result.message);
+    alert(result.message);
+
+    window.location.href = 'http://localhost:3000';
+  } else {
+    console.log(result.message);
+    alert(result.message);
+  }
+}
